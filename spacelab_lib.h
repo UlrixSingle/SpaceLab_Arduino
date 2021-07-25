22fc2ad89290b6999c51a07c2078449a5c3ed6d7
Ulrix Single <yan.sdean@gmail.com>
2021-07-25 23:44:45 +0700

    Add main library " spacelab_lib.h" 2021-07-25

---
+++ spacelab_lib.h
@@ -0,0 +1,11 @@
+#ifndef SPACELAB_LIB_H
+#define SPACELAB_LIB_H
+
+#include <Arduino.h>
+
+#include "./src/Host/host.h"
+#include "./src/Lights/lights.h"
+#include "./src/Manipulator/manipulator.h"
+#include "./src/Module/module.h"
+
+#endif // SPACELAB_LIB_H
